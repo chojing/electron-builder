2d6d9bf5ebd330c1be79ada13a3a90382eedc573
jeunghak.moon <jeunghak.moon@konantech.com>
2021-10-19 18:52:56 +0900

    로그인 프로세스 변경

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    server: null,
     username: null,
-    apikey: null,
-    server: null
+    autologin: false,
+    apikey: null
   },
   mutations: {
     commitServer (state, server) {
@@ -13,8 +14,19 @@ export default createStore({
     commitUsername (state, username) {
       state.username = username
     },
+    commitAutologin (state, autologin) {
+      state.autologin = autologin
+    },
     commitApikey (state, apikey) {
       state.apikey = apikey
+    },
+    clear (state) {
+      state = {
+        server: null,
+        username: null,
+        autologin: false,
+        apikey: null
+      }
     }
   },
   actions: {
@@ -23,6 +35,9 @@ export default createStore({
     },
     setUsername (context, username) {
       context.commit('commitUsername', username)
+    },
+    setAutologin (context, autologin) {
+      context.commit('commitAutologin', autologin)
     },
     setApikey (context, apikey) {
       context.commit('commitApikey', apikey)
