f6a7073c5301fb630ed36e50008289ab13bab035
hyein hwang <hyein.hwang@konantech.com>
2021-10-25 16:58:07 +0900

    수동FTP

--- src/views/ManualFtp.vue
+++ src/views/ManualFtp.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="backBtn" @click="$router.go(-1)"><i class="fas fa-arrow-left"></i></div>
   <section class="manual-ftp-container">
     <div class="wrap">
       <h4 class="tti">수동 FTP</h4>
       <div class="ftp-info mt20">
         <div class="btn-box right">
-          <button @click="terms = false" class="btn h30">등록</button>
-          <button @click="terms = !terms" :aria-pressed="terms ? 'true' : 'false'" id="modify-btn" class="btn blue h30">수정</button>
+          <button @click="userData()" class="btn h30">등록</button>
+          <button @click="modifyFtp()" :aria-pressed="terms ? 'true' : 'false'" id="modify-btn" class="btn blue h30">수정</button>
         </div>
         <select name="" id="selectFtp">
           <option value="FTP정보1">FTP정보1</option>
@@ -17,58 +16,58 @@
         <div class="list flex-center">
           <b>이름</b>
           <div class="flex-1">
-            <input type="text" class="input-box name" value="홍길동" :disabled='isDisabled'>
+            <input v-model="username" ref="usernameInput" type="text" class="input-box name" :disabled='isDisabled'>
           </div>
         </div>
         <div class="list flex-center">
           <b>HOST</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="hostText" :disabled='isDisabled' >
+            <input v-model="userhost" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center">
           <b>PORT</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="portText" :disabled='isDisabled' >
+            <input v-model="userport" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center">
           <b>ID</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="idText" :disabled='isDisabled' >
+            <input v-model="userid" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center">
           <b>PW</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="pwText" :disabled='isDisabled' >
+            <input v-model="userpw" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center">
           <b>DIR</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="dirText" :disabled='isDisabled' >
+            <input v-model="userdir" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center">
           <b>Proxy</b>
           <div class="flex-1">
-            <input type="text" class="input-box" value="proxyText" :disabled='isDisabled' >
+            <input v-model="userproxy" type="text" class="input-box" :disabled='isDisabled' >
           </div>
         </div>
         <div class="list flex-center mode">
           <b>MODE</b>
           <div class="flex-1">
             <div class="radio-input">
-              <input type="radio" name="mode" id="basicVal" checked>
+              <input v-model="basicVal" type="radio" name="mode" id="basicVal" checked>
               <label for="basicVal">기본</label>
             </div>
             <div class="radio-input">
-              <input type="radio" name="mode" id="activeVal" :disabled='isDisabled' >
+              <input v-model="activeVal" type="radio" name="mode" id="activeVal" :disabled='isDisabled' >
               <label for="activeVal">능동형</label>
             </div>
             <div class="radio-input">
-              <input type="radio" name="mode" id="passiveVal" :disabled='isDisabled' >
+              <input v-model="passiveVal" type="radio" name="mode" id="passiveVal" :disabled='isDisabled' >
               <label for="passiveVal">수동형</label>
             </div>
           </div>
@@ -87,14 +86,37 @@
 <script>
 
 export default {
+  name: 'ManualFtp',
   data () {
     return {
-      terms: false
+      terms: false,
+      // 수동FTP 입력값
+      username: '',
+      userhost: '',
+      userport: '',
+      userid: '',
+      userpw: '',
+      userdir: '',
+      userproxy: '',
+      basicVal: '',
+      activeVal: '',
+      passiveVal: ''
     }
   },
   computed: {
     isDisabled: function () {
       return !this.terms
+    }
+  },
+  methods: {
+    userData () {
+      this.terms = false
+      console.log(this.username + ' ' + this.userhost + ' ' + this.userport + ' ' + this.userid + ' ' + this.userpw + ' ' + this.userdir + ' ' + this.userproxy)
+      console.log(this.basicVal + ' ' + this.activeVal + ' ' + this.passiveVal)
+    },
+    modifyFtp () {
+      this.terms = true
+      this.$refs.usernameInput.focus()
     }
   }
 }
