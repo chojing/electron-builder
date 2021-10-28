30d3cba36cbf9b6e1fcced6695a1357c0c203b9c
hyein hwang <hyein.hwang@konantech.com>
2021-10-28 18:49:10 +0900

    연락처팝업 back

--- src/components/main/UserInfo.vue
+++ src/components/main/UserInfo.vue
@@ -100,17 +100,12 @@ export default {
       ipcRenderer.send('closeWindow', this.g_curWindowKey)
     },
     userAdd: function () {
+      this.active = false
+      this.users.push({ username: this.username, usertel: this.usertel })
+      this.username = ''
+      this.usertel = ''
       // console.log('이름 ' + this.username)
       // console.log('번호 ' + this.usertel)
-      if (/^[a-z0-9_-]{8,13}$/.test(this.usertel)) {
-        alert('숫자만 입력해주세요.(8~13자리)')
-        this.usertel.focus()
-      } else {
-        this.active = false
-        this.users.push({ username: this.username, usertel: this.usertel })
-        this.username = ''
-        this.usertel = ''
-      }
     },
     userDel: function (index) {
       this.users.splice(index, 1)
