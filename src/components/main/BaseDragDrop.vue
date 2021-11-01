28fa2b9cd89598b16a27acd951a5f65f874a9d67
jiyoung.choi <jiyoung.choi@konantech.com>
2021-11-01 17:00:14 +0900

    dragdrop file marge

--- src/components/main/BaseDragDrop.vue
+++ src/components/main/BaseDragDrop.vue
@@ -9,55 +9,48 @@
 </template>
 
 <script>
-const electron = window.require('electron')
-const ipcRenderer = electron.ipcRenderer
+const fileList = []
 export default {
   name: 'BaseDragDrop',
   props: {
   },
   data () {
     return {
-      fileList: [],
+    //   fileList: [],
       dataPer: 0
     }
   },
   created () {
-    ipcRenderer.on('open-dialog-result', this.DragDropFile_Result)
   },
   methods: {
     onDrop (event) {
+      console.log('onDrop', event)
       this.dataPer = 0
       this.DragDropFile(event.dataTransfer.files)
-      console.log('onDrop', event)
     },
     onChange (event) {
       console.log('onChange')
       this.dataPer = 0
-      console.log('file', event)
+      this.DragDropFile(event.target.files)
     },
     DragDropFile (files) {
       console.log(files)
       if (files.length) {
         for (let i = 0; i < files.length; i++) {
+          const inputfile = {
+            fileName: '',
+            key: '',
+            path: '',
+            size: 0
+          }
           const file = files[i]
-          this.fileList.push(file.path)
-          console.log(this.fileList)
+          inputfile.path = file.path
+          inputfile.fileName = file.name
+          inputfile.size = file.size
+          fileList.push(inputfile)
         }
-        const convertFileList = this.fileList.map(f => f) // serialize 조건 충족
-        ipcRenderer.send('drag-file', convertFileList)
-        // this.fileList = []
+        this.$emit('valueReturn', fileList)
       }
-    },
-    DragDropFile_Result (event, _isCancel, _fileData) {
-      if (_isCancel == true) {
-        this.$emit('valueReturn', '취소하였습니다.')
-        return
-      }
-      if (_fileData === undefined) {
-        this.$emit('valueReturn', '파일을 가져오는 중 에러가 발생했습니다.')
-        return
-      }
-      this.$emit('valueReturn', _fileData)
     }
   }
 }
