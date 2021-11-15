4b8b1df576deecf2c3d3ed142215104a6cb21ca5
eunju.kwak <eunju.kwak@konantech.com>
2021-11-15 16:10:41 +0900

    * Main   > 사용자 즐겨찾기 조회   > 즐겨찾기 컨텍스트메뉴 추가     - 즐겨찾기한 node일 경우 즐겨찾기 체크박스 체크 * Template_context_menu   > 즐겨찾기 컨텍스트 메뉴     - 체크 유무에 따라 즐겨찾기 추가/삭제(진행중) * Template_tree   > 조회할 서버 아이디가 없을 경우 alert창   > p태그에 즐겨찾기 추가/삭제를 위한 data-nodeid 추가

---
+++ src/components/main/Template_context_menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul id = "favorits-menu">
+    <li class = "favorits-menu-list" :nodeid="nodeid">
+      즐겨찾기
+      <input id = "favorits-checkbox-id" type="checkbox" class ="ml10" @click="editFavoritsMenu($event)"/>
+    </li>
+  </ul>
+</template>
+
+<script>
+// const axios = require('@/assets/js/axios.js')
+export default {
+  name: 'Template_favorits_menu',
+  props: {
+    username: String,
+    nodeid: String
+  },
+  methods: {
+    editFavoritsMenu: function (e) {
+      var isFavorits = document.getElementById('favorits-checkbox-id').checked
+      // var alias = '테스트'
+      if (isFavorits) { // 즐겨찾기 추가
+        alert('체크박스 체크 되어있음 ', isFavorits)
+        // axios.postAsyncAxios('/v2/users/' + this.username + '/favorits/' + this.nodeid, alias, null, (response) => {
+        //   console.log('post', response)
+        // })
+      } else if (!isFavorits) { // 즐겨찾기 삭제
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
