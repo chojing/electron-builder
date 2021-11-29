669b28ec3557431372f39ad8b533490da608c04e
eunju.kwak <eunju.kwak@konantech.com>
2021-11-29 14:42:10 +0900

    * 전송페이지   > FTPSET 시 site 전송 모드, sever 전송 모드 분기 * 수동FTP 관리 페이지   > ftpserver 전송모드 DB값에 따라 radio버튼 체크   > 저장, 수정 시 radio버튼 mode값 저장

--- src/components/fileUpload/Template_file.vue
+++ src/components/fileUpload/Template_file.vue
@@ -85,6 +85,7 @@ export default {
     return {
       g_windowIndex: 0,
       selfKey: '',
+      c_ftpmode: [],
       targetFtpInfo: '',
       fileList: [],
       telValue: [],
@@ -104,9 +105,12 @@ export default {
     init: function (event, key, data, type) {
       if (type == 'init') {
         this.targetFtpInfo = data
-        this.ftpSet(data.site, data.serverlist)
         this.selfKey = key
         this.g_curWindowKey = key
+        axios.getAsyncAxios('/v2/ftpserver/code', {}, (response) => {
+          this.c_ftpmode = response.data.c_ftpmode
+          this.ftpSet(data.site, data.serverlist)
+        })
         // const curFtpServer = { host: data.value.userhost, port: data.value.userport, user: data.value.userid, password: data.value.userpw, serverName: data.value.username, homeDir: data.value.userdir }
         console.log('ftp 정보 : ', custom.proxy2map(this.targetFtpInfo))
       } else if (type == 'userTelData') {
@@ -272,9 +276,11 @@ export default {
       if (site) {
         ftpSite.connectionType = '1'
         ftpSite.siteName = site.name
+        ftpSite.connectionType = site.mode_code
       } else {
         ftpSite.connectionType = '1'
         ftpSite.siteName = 'konanSite'
+        ftpSite.connectionType = 'sequential'
       }
       for (let server of serverlist) {
         // activeIp:
@@ -291,7 +297,11 @@ export default {
         curFtpServer1.password = server.password
         curFtpServer1.name = server.name
         curFtpServer1.rootpath = server.rootpath
-        curFtpServer1.passive = true // passive : true / active : false
+        if (custom.code.valueToCode(this.c_ftpmode, server.mode) === 'active') {
+          curFtpServer1.passive = false // passive : true / active : false
+        } else {
+          curFtpServer1.passive = true // 모드 : 기본, 수동 분기될 시 변경해야함
+        }
         curFtpServer1.parentSiteName = ftpSite.siteName
         curFtpServer1.activeIp = 'default'
         ftpSite.ftpServerList.push(curFtpServer1)
@@ -309,7 +319,6 @@ export default {
         // curFtpServer2.activeIp = ''
         // ftpSite.ftpServerList.push(curFtpServer2)
       }
-
       g_ftpSendData.ftpSite = ftpSite
     },
     doCancel: function () {
--- src/components/manual/ManualFtp.vue
+++ src/components/manual/ManualFtp.vue
@@ -58,16 +58,16 @@
           <b>MODE</b>
           <div class="flex-1">
             <div class="radio-input">
-              <input v-model="ftpInfo.mode" value="기본" type="radio" name="mode" id="basicVal" checked>
-              <label for="basicVal"> 기본</label>
+              <input v-model="ftpInfo.mode" value="normal" type="radio" name="mode" id="normal" :disabled='isDisabled'>
+              <label for="normal"> 기본</label>
             </div>
             <div class="radio-input">
-              <input v-model="ftpInfo.mode" value="능동형" type="radio" name="mode" id="activeVal" :disabled='isDisabled' >
-              <label for="activeVal"> 능동형</label>
+              <input v-model="ftpInfo.mode" value="active" type="radio" name="mode" id="active" :disabled='isDisabled' >
+              <label for="active"> 능동형</label>
             </div>
             <div class="radio-input">
-              <input v-model="ftpInfo.mode" value="수동형" type="radio" name="mode" id="passiveVal" :disabled='isDisabled' >
-              <label for="passiveVal"> 수동형</label>
+              <input v-model="ftpInfo.mode" value="passive" type="radio" name="mode" id="passive" :disabled='isDisabled' >
+              <label for="passive"> 수동형</label>
             </div>
           </div>
         </div>
@@ -81,10 +81,7 @@
 </template>
 
 <script>
-const electron = window.require('electron')
-const ipcRenderer = electron.ipcRenderer
-// eslint-disable-next-line no-unused-vars
-const axios = require('@/assets/js/axios.js')
+const { axios, ipcRenderer } = require('@/assets/js/include.js')
 export default {
   name: 'ManualFtp',
   data () {
@@ -94,6 +91,7 @@ export default {
       isOptionClicked: false, /* true -> 취소/저장 버튼 보이기 , false -> 추가,수정/닫기 버튼 보이기 */
       g_curWindowKey: '',
       parentKey: '',
+      c_ftpmode: [],
       // 수동 FTP 입력값
       ftpInfo: {
         ftpserverid: '',
@@ -104,7 +102,7 @@ export default {
         password: '',
         rootpath: '',
         proxy: '',
-        mode: 'active',
+        mode: '',
         ismanual: 1,
         owner: 'konan',
         macvolume: '',
@@ -138,8 +136,10 @@ export default {
       param.limit = 0
       param.offset = 0
       console.log('condition : ', condition)
+      // axios.getAsyncAxios('/v2/ftpserver/code', {}, (response) => {
+      //   this.c_ftpmode = response.data.c_ftpmode
       axios.getAsyncAxios('/v2/ftpservers', param, (response) => {
-        // console.log(response)
+        console.log('결과', response.data)
         this.addSelect = response.data.results
         this.ftpSelected = this.addSelect[0].name
         this.ftpInfo.name = this.addSelect[0].name
@@ -149,8 +149,11 @@ export default {
         this.ftpInfo.password = this.addSelect[0].password
         this.ftpInfo.rootpath = this.addSelect[0].rootpath
         this.ftpInfo.proxy = this.addSelect[0].proxy
+        this.ftpInfo.mode = this.addSelect[0].mode_code
+        // this.ftpInfo.mode = custom.code.valueToCode(this.c_ftpmode, this.addSelect[0].mode)
         this.ftpInfo.ftpserverid = this.addSelect[0].ftpserverid
       })
+      // })
     },
     selected: function () {
       // console.log('들어있는 값 확인 : ', this.addSelect)
@@ -166,6 +169,8 @@ export default {
           this.ftpInfo.password = this.addSelect[index].password
           this.ftpInfo.rootpath = this.addSelect[index].rootpath
           this.ftpInfo.proxy = this.addSelect[index].proxy
+          this.ftpInfo.mode = this.addSelect[index].mode_code
+          // this.ftpInfo.mode = custom.code.valueToCode(this.c_ftpmode, this.addSelect[index].mode)
           this.ftpInfo.ftpserverid = this.addSelect[index].ftpserverid
         }
       })
@@ -195,6 +200,7 @@ export default {
       ftpInfoData.rootpath = this.ftpInfo.rootpath
       ftpInfoData.proxy = this.ftpInfo.proxy
       ftpInfoData.mode = this.ftpInfo.mode
+      // ftpInfoData.mode = custom.code.codeToValue(this.c_ftpmode, this.ftpInfo.mode)
       ftpInfoData.ismanual = this.ftpInfo.ismanual
       ftpInfoData.owner = this.ftpInfo.owner
       ftpInfoData.macvolume = this.ftpInfo.macvolume
@@ -242,6 +248,7 @@ export default {
       this.ftpInfo.password = ''
       this.ftpInfo.rootpath = ''
       this.ftpInfo.proxy = ''
+      this.ftpInfo.mode = 'normal'
       this.ftpInfo.ftpserverid = '-1'
       // console.log('사용자지정 ', this.ftpInfo)
 
